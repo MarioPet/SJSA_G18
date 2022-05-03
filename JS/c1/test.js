c0a5e3bfe555b6077d10d18d33f5de3fb0a90bd9
Xellaus <Xellaus>
2022-05-03 21:51:43 +0200

    Prv cas JS

---
+++ JS/c1/test.js
@@ -0,0 +1,28 @@
+console.log ("Hello World!+")
+console.log("Semos")
+console.log("Academy")
+
+var greeting
+
+greeting = "Hello!"
+
+console.log(greeting)
+
+const a = 5
+
+
+console.log(a)
+
+
+let ime = "Alek"
+
+// Dali telepatija postoi?
+
+console.log (ime,a,greeting)
+
+console.warn ("Pecati vo zolto")
+console.error ("Pecativo crveno!")
+
+let a=5,c=a+a
+
+console.log(c)
