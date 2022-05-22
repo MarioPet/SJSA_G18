fb094a7df2f2c9471fcbce6c7139f5ab3115f2a0
Xellaus <Xellaus>
2022-05-23 01:03:08 +0200

    first step Homework9

--- JS/d9/script-server.js
+++ JS/d9/script-server.js
@@ -7,6 +7,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then(dataUsers => {
 
         get_usable_user_data(dataUsers)
+        generateHTMLElementsForUserData(get_usable_user_data(dataUsers))
 
     })
 
@@ -20,12 +21,14 @@ function get_usable_user_data(data) {
     console.log(data)
 
     let arrayAddress = (Object.values(data[0].address))
-    let userAddress = (arrayAddress[2] + ' City, ' + arrayAddress[0] + ' street, ' + arrayAddress[1])
+    let userAddress = (arrayAddress[2] + ' City, ' + arrayAddress[0] + ' st., ' + arrayAddress[1])
 
     let userName = data[0].name
     let userEmail = data[0].email
     let userWebsite = data[0].website
     let array = []
+
+    //declaring the variables, and seeing the logic of where is the //value
 
     for (let i = 0; i < data.length; i++) {
 
@@ -34,24 +37,14 @@ function get_usable_user_data(data) {
     
         userName = data[i].name
         userEmail = data[i].email
-        userWebsite = data[i].website
-
-
-
-       
-       
+        userWebsite = data[i].website     
         array[i] = [userName,userAddress,userEmail,userWebsite]
+
+        
+        
     }
-
-   
-
-    
-
-
-
-
-
     console.log(array)
+    return array
 }
     //Kreiraj karta za:
     //ime
@@ -62,6 +55,24 @@ function get_usable_user_data(data) {
 
 
     //mi treba data[x].adress
+
+
+    function generateHTMLElementsForUserData (data) {
+        var htmlContainer = document.getElementById("container")
+        var htmlCard = document.getElementById('card')
+        var newCard = document.createElement('div.card')
+        
+
+        for (let c = 0; c < data[0].length; c++) {
+            htmlContainer.append(document.createElement('div.card'))
+            console.log(c)
+        }
+
+        
+        var htmlNamePara = document.createElement("p")
+        htmlNamePara.innerText = data[0]
+        htmlCard.appendChild(htmlNamePara)
+    }
 
 
 
